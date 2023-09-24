7ffb3e583bf7f342edc94083db618ae4b5f9c704
sooreyshh <sureshputti.cris@gmail.com>
2023-09-24 18:13:13 +0530

    code commit - 3
    code commit - 3

--- src/pages/Home/HomePage.component.js
+++ src/pages/Home/HomePage.component.js
@@ -6,26 +6,68 @@ import Footer from "./Footer.component";
 import HowToOrder from "./HowToOrder.component";
 import HowToOrderGuide from "./HowToOrderGuide.component";
 import RailTools from "./RailTools.component";
+import PNRInfo from "../PNRInfo/PNRInfo.component";
+import OrderConfirmation from "../OrderConfirmation/OrderConfirmation.component";
+import Restaurants from "../PNRInfo/Restaurants.component";
 import Testimonials from "./Testimonials.component";
 import TravelBlog from "./TravelBlog.component";
 import VendorRegistration from "./VendorRegistration.component";
+import Login from "../Login/Login.component";
+import TrackOrder from "../TrackOrder/TrackOrder.component";
+
 const HomePage = () => {
-  return (
-    <>
-      
-      <Banner></Banner>
-      <div className="ritekhana-main-content home-page">
-        <HowToOrder></HowToOrder>
-        <RailTools></RailTools>
-        <Testimonials></Testimonials>
-        <VendorRegistration></VendorRegistration>
-        <DownloadLinks></DownloadLinks>
-        <TravelBlog></TravelBlog>
-      </div>
-      <HowToOrderGuide></HowToOrderGuide>
-     
-    </>
-  );
+  console.log("====HomePage======")
+  const _token = localStorage.getItem("token");
+  console.log("====_token======"+_token)
+  if(_token)
+  {
+    return (
+      <>
+        
+        <Banner></Banner>
+        <div className="ritekhana-main-content home-page">
+         {/* <PNRInfo></PNRInfo>     
+        <OrderConfirmation></OrderConfirmation>     
+        <Restaurants></Restaurants>   
+        <TrackOrder></TrackOrder>     
+               */}
+          
+          <HowToOrder></HowToOrder>         
+          <RailTools></RailTools>
+          <Testimonials></Testimonials>
+          <VendorRegistration></VendorRegistration>
+          <DownloadLinks></DownloadLinks>
+          <TravelBlog></TravelBlog>
+        </div>
+        <HowToOrderGuide></HowToOrderGuide>
+       
+      </>
+    );
+  }
+  else
+  {
+    return (
+      <>
+        
+         <Banner></Banner> 
+        <div className="ritekhana-main-content home-page">
+        {/* <PNRInfo></PNRInfo>     
+        <OrderConfirmation></OrderConfirmation>     
+        <Restaurants></Restaurants>            */}
+          <Login></Login>  
+          <HowToOrder></HowToOrder>       
+          <RailTools></RailTools>
+          <Testimonials></Testimonials>
+          <VendorRegistration></VendorRegistration>
+          <DownloadLinks></DownloadLinks>
+          <TravelBlog></TravelBlog>
+        </div>
+        <HowToOrderGuide></HowToOrderGuide>
+       
+      </>
+    );
+  }
+ 
 };
 
 export default HomePage;
